18b79c1b2a971aa98e6b47e4544a53508268e4c1
Arlukin <daniel@cybercow.se>
2022-04-10 00:37:20 +0200

    chore: add tools, prettier, gitignore, nvmrc etc

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import reactRefresh from "@vitejs/plugin-react-refresh";
+import ViteRadar from "vite-plugin-radar";
+import visualizer from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
-})
+export default defineConfig(({ mode }) => {
+  return {
+    server: {
+      port: 3000,
+    },
+    build: {
+      sourcemap: true,
+    },
+    plugins: [
+      reactRefresh(),
+      visualizer({
+        open: false,
+        gzipSize: true,
+        brotliSize: true,
+      }),
+      ViteRadar({
+        // Google Analytics tag injection
+        analytics: {
+          id: "xxxx",
+        },
+      }),
+    ],
+  };
+});
