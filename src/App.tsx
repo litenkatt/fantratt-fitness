5ddcbcb0f69ef7190e3d3fdcc3c75f8eb15e91e2
Arlukin <daniel@cybercow.se>
2022-04-10 00:37:26 +0200

    fix: yarn prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
-import fflogo from './res/images/fflogo.svg'
-import './App.css'
-import WordArt from 'react-wordart'
-import styled from 'styled-components'
+import React from "react";
+import fflogo from "./res/images/fflogo.svg";
+import "./App.css";
+import WordArt from "react-wordart";
+import styled from "styled-components";
 
 const StyledApp = styled.div`
-background: linear-gradient(180deg, rgba(120,33,68,1) 0%, rgba(24,0,10,1) 55%, rgba(255,214,230,1) 100%);
-height: 100vh`;
+  background: linear-gradient(
+    180deg,
+    rgba(120, 33, 68, 1) 0%,
+    rgba(24, 0, 10, 1) 55%,
+    rgba(255, 214, 230, 1) 100%
+  );
+  height: 100vh;
+`;
 
 function App() {
-
   return (
     <StyledApp className="App">
-        <img src={fflogo} className="App-logo" alt="logo" />
-        <WordArt text='Fåntratt Fitness' theme={`italicOutline`} fontSize={100} />
+      <img src={fflogo} className="App-logo" alt="logo" />
+      <WordArt text="Fåntratt Fitness" theme={`italicOutline`} fontSize={100} />
     </StyledApp>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/react-wordart.d.ts
+++ src/react-wordart.d.ts
@@ -1,1 +1,1 @@
-declare module 'react-wordart';
+declare module "react-wordart";
